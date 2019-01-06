aa215feebde4dbe03a5e8af9d9a2d6334bb8127a
jj97181818 <jj97181818@gmail.com>
2019-01-06 23:49:17 +0800

    start

---
+++ app/src/androidTest/java/me/jj97181818/dangongcha/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package me.jj97181818.dangongcha;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("me.jj97181818.dangongcha", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/me/jj97181818/dangongcha/MainActivity.java
@@ -0,0 +1,13 @@
+package me.jj97181818.dangongcha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
