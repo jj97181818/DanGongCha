4f780099fd231cabc437e368429f058606bd3b84
Pin Lin <moneycat711@gmail.com>
2019-01-10 14:35:28 +0800

    Align comment

--- app/src/main/java/me/jj97181818/dangongcha/api/Infos.java
+++ app/src/main/java/me/jj97181818/dangongcha/api/Infos.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Infos implements Serializable {
     public List<Route> routes;
     public class Route {
-        public String routeUID;    //路線辨識碼
-        public String routeName;   //路線名稱
-        public String city;    //城市英文名稱
-        public String departureStopName;   //起站名稱
-        public String destinationStopName; //終站名稱
+        public String routeUID;             //路線辨識碼
+        public String routeName;            //路線名稱
+        public String city;                 //城市英文名稱
+        public String departureStopName;    //起站名稱
+        public String destinationStopName;  //終站名稱
     }
 }
 
