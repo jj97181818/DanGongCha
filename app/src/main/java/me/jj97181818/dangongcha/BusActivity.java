041e516b973d2953a351b2109822967d9d743754
jj97181818 <jj97181818@gmail.com>
2019-01-10 17:38:30 +0800

    Add TabLayout and ViewPager

---
+++ app/src/main/java/me/jj97181818/dangongcha/BusActivity.java
@@ -0,0 +1,23 @@
+package me.jj97181818.dangongcha;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+
+        private TabLayout mTblTitle;
+
+
+        private AddNewContactFragment addNewContactFragment;
+        private SearchContactFragment searchContactFragment;
+
+//        Intent it = getIntent();
+//        int = it.getStringExtra("");
+    }
+}
